e3c9f88683a747c27ed0e22654e018ab3f5ae81d
Asif Ahmed <asifuzzaman.ahmed@gmail.com>
2016-06-24 15:05:33 -0700

    Add Routes Folder

---
+++ server/routes/view-routes.js
@@ -0,0 +1,5 @@
+module.exports = function(app, express) {
+	app.get('/', function(req, res) {
+		res.send('cows are here to party')
+	})
+}
--- server/siloServer.js
+++ server/siloServer.js
@@ -1,2 +1,14 @@
 var express = require('express');
-var router = require('passport');
+var router = express.Router();
+
+var app = express();
+
+var config = require('../env/client-config');
+var ServicesController = require('./util/services/ServicesController');
+
+//Routes 
+require('./routes/view-routes.js')(app);
+
+app.listen(3030, function() {
+  console.log('Silo Server Operational on port 3030')
+}); 
