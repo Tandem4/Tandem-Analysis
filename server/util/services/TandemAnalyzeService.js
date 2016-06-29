de8396ada898989e7f11855b87f366df789eb16c
Asif Ahmed <asifuzzaman.ahmed@gmail.com>
2016-06-28 17:55:01 -0700

    Delete Extra Files

--- server/util/services/TandemAnalyzeService.js
+++
@@ -1,9 +0,0 @@
-
-module.exports = {
-	// 1) Add General Tone
-  createGeneralTone: function(textObj) {
-  	var generalTone = parseInt(textObj['anger']) + parseInt(textObj['disgust']) + parseInt(textObj['fear']) + parseInt(textObj['happiness']) + parseInt(textObj['sadness']);
-  	textObj['generalTone'] = generalTone/5; 
-  	console.log('expect generalTone in textObj', textObj);
-  }
-}
