efe20782b2477a4b47e43d623bf66998a9b97516
Asif Ahmed <asifuzzaman.ahmed@gmail.com>
2016-06-28 17:50:39 -0700

    Add Sucker Free

--- app/store.js
+++ app/store.js
@@ -4,6 +4,7 @@ import { ListView } from 'react-native';
 import rootReducer from './rootReducer';
 import articlesData from './data/articlesData';
 
+let sucker = 'sucker free';
 // for ListView 
 let dataSource = new ListView.DataSource({
   rowHasChanged: (row1, row2) => row1 !== row2,
